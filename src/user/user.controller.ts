a5da5463338429aa35ff41cb79cb295c6266bb5f
Jhonnatan <jhonnatan.silva@fatec.sp.gov.br>
2019-03-29 13:46:33 -0300

    inserted update and validationpipe in user.service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -24,7 +24,6 @@ export class UserController {
   async findAll(@Query() query: UserDto, @Res() res): Promise<User> {
     try {
       const posts = await this.userService.findAll();
-
       return res.status(HttpStatus.OK).json(posts);
     } catch (e) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
@@ -32,34 +31,47 @@ export class UserController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<string> {
-    return 'findOne';
+  async findOne(@Param('id') id: string, @Res() res): Promise<string> {
+    try {
+      console.log(id);
+      const post = await this.userService.findOne(id);
+      return res.status(HttpStatus.OK).json(post);
+    } catch (e) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
+    }
   }
 
   @Post()
   @UsePipes(ValidationPipe)
   async create(@Body() userDto: UserDto, @Res() res): Promise<User> {
     try {
-      const posts = await this.userService.create(userDto);
-      return res.status(HttpStatus.OK).json(posts);
+      const post = await this.userService.create(userDto);
+      return res.status(HttpStatus.OK).json(post);
     } catch (e) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
     }
   }
 
   @Put(':id')
+  @UsePipes(ValidationPipe)
   async update(
     @Param('id') id: string,
     @Body() userDto: UserDto,
-  ): Promise<string> {
-    return 'update';
+    @Res() res,
+  ): Promise<User> {
+    try {
+      const post = await this.userService.update(id, userDto);
+      return res.status(HttpStatus.OK).json(post);
+    } catch (e) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
+    }
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string, @Res() res): Promise<User> {
     try {
-      const posts = await this.userService.remove(id);
-      return res.status(HttpStatus.OK).json(posts);
+      const post = await this.userService.remove(id);
+      return res.status(HttpStatus.OK).json(post);
     } catch (e) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
     }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './interfaces/user.interface';
@@ -16,9 +16,27 @@ export class UserService {
     }
   }
 
+  async findOne(id: string): Promise<any> {
+    try {
+      return await this.userModel.findById(id);
+    } catch (e) {
+      return e;
+    }
+  }
+
+  @UsePipes(ValidationPipe)
   async create(userDto: UserDto): Promise<any> {
     try {
       return await this.userModel.create({ ...userDto });
+    } catch (e) {
+      return e;
+    }
+  }
+
+  @UsePipes(ValidationPipe)
+  async update(id: string, userDto: UserDto): Promise<any> {
+    try {
+      return await this.userModel.findByIdAndUpdate(id, userDto);
     } catch (e) {
       return e;
     }
